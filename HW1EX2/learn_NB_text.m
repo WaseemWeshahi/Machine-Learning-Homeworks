2bbb2b51fda43da65d22d0f96b9cf9cf1f84a77b
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-12-18 04:05:35 +0200

    first draft of HW1EX2
    F

--- HW1EX2/learn_NB_text.m
+++ HW1EX2/learn_NB_text.m
@@ -1,29 +1,34 @@
 function [Pw,P] = learn_NB_text
-format long
+% INPUT: the text attributes saved in corpus_train.mat
+% OUTPUT:
+%   Pw - class conditional probabilities matrix: a |voc| x #OfCategories that Pw(i,j) is the probability of the word
+% i showing up in the class j, aka P(Wi|Cj)
+%   P - class Priors vector: a 1 x #OfCategories vector so that P(j) is the
+% prior of the class j
+%
+
 m = matfile('corpus_train.mat');
 texAll = m.texAll;
 cat = m.cat;
 lbAll = m.lbAll;
 voc = m.Voc;
-P = zeros(1,length(m.cat)); 
 
 wordsCount=0;
 
-% Calculating Priors of each class
+P = zeros(1,length(cat)); 
+Pw = zeros(length(voc),length(cat));
 for j=1:length(cat)
-    sum=0;
+    % Calculating Priors
+    sum=0; % The number of words labeled j (counting duplicates)
     for i =1:length(lbAll)
-        if(ismember(cat{j,1},lbAll{i,1})) 
+        if(ismember(cat{j,1},lbAll{i,1})) % equavilant of ==
            sum=sum+length(texAll{i,1});
         end
     end
     P(j)=sum;
-    wordsCount=wordsCount+sum;
-end
-P = P/wordsCount; %to get the probabilities
+    P = P/wordsCount; %to get the probabilities
 
-Pw = zeros(length(voc),length(cat));
-for j=1:length(cat)
+    wordsCount=wordsCount+sum;
     % cls is a cell array, each cell containing a line that belongs to the
     % category j
     textj = texAll(ismember(lbAll,cat(j)));
